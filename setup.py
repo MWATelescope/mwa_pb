ac4d731ac25747cd947893c9468bd4cc4df2be40
Andrew Williams <Andrew.Williams@curtin.edu.au>
2018-05-16 16:06:46 +0800

    Add data files to setup.py so they get installed too.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
   name='mwa_pb',
   version='1.1.0',
   packages=['mwa_pb'],
+  package_data={'mwa_db':['data/*.fits', 'data*.txt']},
   url='https://github.com/MWATelescope/mwa_pb',
   license='GPLv3',
   author='MWA Team members, repo managed by Andrew Williams',
