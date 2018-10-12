3ab5fe5c515cb7b2144465282875726ad06708c7
Christopher Jordan <christopherjordan87@gmail.com>
2018-10-12 10:41:01 +0800

    Fix urllib imports for python2 and 3.

--- mwa_pb/metadata.py
+++ mwa_pb/metadata.py
@@ -1,8 +1,17 @@
 """Library to access the MWA metadata web services
 """
 
-import urllib.request, urllib.parse, urllib.error
-import urllib.request, urllib.error, urllib.parse
+# Python3
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+    from urllib.error import HTTPError, URLError
+
+# Python2
+except ImportError:
+    from urllib import urlencode
+    from urllib2 import urlopen, HTTPError, URLError
+
 import json
 
 # Append the service name to this base URL, eg 'con', 'obs', etc.
@@ -16,16 +25,16 @@ def getmeta(servicetype='metadata', service='obs', params=None):
        and a set of parameters as a Python dictionary, return a Python dictionary containing the result.
     """
     if params:
-        data = urllib.parse.urlencode(params)  # Turn the dictionary into a string with encoded 'name=value' pairs
+        data = urlencode(params)  # Turn the dictionary into a string with encoded 'name=value' pairs
     else:
         data = ''
     # Get the data
     try:
-        result = json.load(urllib.request.urlopen(BASEURL + servicetype + '/' + service + '?' + data))
-    except urllib.error.HTTPError as error:
+        result = json.load(urlopen(BASEURL + servicetype + '/' + service + '?' + data))
+    except HTTPError as error:
         print(("HTTP error from server: code=%d, response:\n %s" % (error.code, error.read())))
         return
-    except urllib.error.URLError as error:
+    except URLError as error:
         print(("URL or network error: %s" % error.reason))
         return
     # Return the result dictionary
