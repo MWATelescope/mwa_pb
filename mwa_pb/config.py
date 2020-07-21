643b7d00f1a81907f211f625a0c17cdfe49abb50
Nick Swainston <nickaswainston@gmail.com>
2020-07-21 13:20:23 +0800

    Instead of using git-lfs, config.py downloads the h5 file

--- mwa_pb/config.py
+++ mwa_pb/config.py
@@ -7,14 +7,30 @@ It can either be the simple analytic model or the more advanced semi-analytic mo
 # import measured_beamformer,mwapb
 
 import os
+import logging
 
 from astropy.coordinates import EarthLocation
+
+# configure the logging
+logging.basicConfig(format='# %(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger(__name__)
+
 
 datadir = os.path.join(os.path.dirname(__file__), 'data')
 
 # 2016 beam model
 h5file = os.path.join(datadir, 'mwa_full_embedded_element_pattern.h5')
 h5fileversion = "UNDEFINED"
+if not os.path.exists(h5file):
+    # Importing download functions here to avoid unnessiary imports when the file is available
+    import urllib.request
+    logger.info("The mwa_full_embedded_element_pattern.h5 file does not exist. Downloading it from http://ws.mwatelescope.org")
+    response = urllib.request.urlopen("http://ws.mwatelescope.org/static/mwa_full_embedded_element_pattern.h5", timeout = 5)
+    content = response.read()
+    f = open(h5file, 'wb' )
+    f.write( content )
+    f.close()
+    logger.info("Download complete")
 
 __version__ = "1.2.0"
 
